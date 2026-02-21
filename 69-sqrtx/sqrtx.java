f7b262da769ce23d5d7f01876f99c83e3aa5c9c3
RagavanR19 <ragavanrv19@gmail.com>
2026-02-21 22:55:46 +0530

    Time: 1 ms (96.93%) | Memory: 42.7 MB (61.85%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,14 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0){
-            return x;
-        }
-        double num = x,prev = 0;
-
-        while(Math.abs(num - prev)>1e-10){
-            prev = num;
-            num = 0.5*(num+x/num);
-        }
-        return (int)num;
+        //Test run
+        return (int)Math.sqrt(x);
     }
 }
